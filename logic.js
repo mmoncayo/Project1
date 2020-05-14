e17ce7184cf37f03bffc745ce065b359cb3209b3
Sheena <sheenatran@gmail.com>
2020-05-13 21:13:25 -0700

    updated layout and styling and added image

--- logic.js
+++ logic.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
-
+    var jobKeyword = $("#keyword").val().trim();
+    var location = $("#cityWeather").val().trim().split(" ").join("+");
+    //var jobQuery= "https://jobs.github.com/positions.json?description=" + jobKeyword + "&location=" + location;
+    function jobSearch(jobQuery) {
+        console.log("jobQuery: ", jobQuery);
+        $.ajax({
+            url: jobQuery,
+            method: "GET",
+            dataType: 'jsonp',
+        })
+            .then(function (jobData) {
+                // confirm if ajax call pulled the right data (i.e., job openings and city location) 
+                console.log(jobData.title);
+                console.log(jobData.location);
+            })
+    }
     var authKey = "60491f0ae881db1d21b8fc251fe7e947"
-
     function runQuery(newLink) {
         console.log("newLink", newLink)
-
         $.ajax({
             url: newLink,
             method: "GET"
         })
             .then(function (WeatherData) {
                 // $("#weatherSection").empty();
-
                 //start dumping to html
                 var sectionCard = $("<div>");
                 sectionCard.addClass("card");
@@ -22,10 +34,8 @@ $(document).ready(function () {
                 $("#articleCard").append("<h3>" + ((WeatherData.list[1].main.temp - 273.15) * 9 / 5 + 32).toFixed(2) + " </h3>");
                 $("#articleCard").append("<h3>" + WeatherData.city.name + "</h3>");
                 // how can i add a class to the newly appended <h3>
-
             });
     }
-
     $("#search").on("click", function (event) {
         event.preventDefault();
         // get search term
@@ -34,18 +44,17 @@ $(document).ready(function () {
         //     queryTerm = queryTerm.join("+");
         //   };
         console.log(queryTerm);
-
+        var jobTerm = $("#keyword").val();
         //add in the search term
         var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + queryTerm + "&appid=" + authKey;
-
+        var jobQueryURL = "https://jobs.github.com/positions.json?description=" + jobTerm + "&location=" + queryTerm;
         runQuery(queryURL);
+        jobSearch(jobQueryURL);
     })
     $("#clear").on("click", function (event) {
-        event.prebentDefault();
+        event.preventDefault();
         $("#citySection").empty();
         $("#weatherSection").empty();
-
-
+        $("#jobCity").empty();
     })
-
 });
